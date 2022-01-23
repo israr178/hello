f97bc4c1b2693c29fb796c062d2ca3a624cae392
Israr <muhammadisrar.ahmad178@gmail.com>
2022-01-23 21:06:06 +0500

    initial commit

---
+++ belongApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  belongApp
+//
+//  Created by Israr Ahmed on 23/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ belongApp/belongAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  belongAppApp.swift
+//  belongApp
+//
+//  Created by Israr Ahmed on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct belongAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
